3d5664f0da32c5a18e79d102a57a3546f350c119
DomainK <92905354+DomainK@users.noreply.github.com>
2022-03-04 16:19:36 +0900

    Add files via upload

---
+++ 12/textquery/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "TextQuery.h"
+
+void runQueries(std::ifstream& infile);
+
+int main() {
+	std::ifstream fin("file.txt");
+	runQueries(fin);
+
+	return 0;
+}
+
+void runQueries(std::ifstream& infile) {
+	TextQuery tq(infile);
+	while (true) {
+		std::cout << "enter word to look for, or q to quit: ";
+		std::string s;
+		if (!(std::cin >> s) || s == "q") break;
+		print(std::cout, tq.query(s)) << std::endl;
+	}
+}
