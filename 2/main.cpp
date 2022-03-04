0cb942c62f310a28fcb46232c172e27cde90df7b
DomainK <92905354+DomainK@users.noreply.github.com>
2022-03-04 16:14:02 +0900

    Add files via upload

---
+++ 2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Sales_data.h"
+#include <string>
+#include <vector>
+using std::cout; using std::cin;
+using std::vector;
+using std::string;
+
+int main()
+{
+	string c("Thank you");
+	for (auto i : c)
+		
+
+	return 0;
+}
